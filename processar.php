2e98794250179621ebdaa1d706bee4ae3ce2d6b8
drigo <152926299+drigorios@users.noreply.github.com>
2023-12-08 22:51:37 -0300

    Add files via upload
    bosta de github

--- processar.php
+++ processar.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oficina do Drigo</title>
 </head>
-
 <body>
     <h1>Oficina do Drigo</h1>
     <h2>Pedidos:</h2>
     <?php
+     //variáveis:
      $pneu = $_POST ['pneu'];
      $oleo = $_POST ['oleo'];
      $velas = $_POST ['velas'];
@@ -18,11 +18,10 @@
     echo '<p>Processo processado ás:'
     .date('H:i, d/m/y')
     .'</p>';
-    
+    //quantidades abaixo:
     echo $pneu .' pneus <br>';
     echo $oleo .' óleos <br>';
     echo $velas .' svelas <br>';
-    
     ?>
     <a href="index.php">Voltar</a>
 </body>
